542f9cec84464e12375562cefb56ad9cbf50e364
yKesamaru <y.kesamaru@tokai-kaoninsho.com>
2023-12-14 13:16:43 +0900

    Update README.md with explanations and installation instructions

--- README.md
+++ README.md
@@ -24,43 +24,37 @@ Ubuntuは2年おきにLTS(Long Term Support)版がリリースされます。
 
 ### ins.sh
 コメントに説明が書いてありますので、それぞれ参照いただくとして、説明が必要な部分を解説します。
+
+https://github.com/yKesamaru/reinstall_system/blob/dcbf24f9259d4ea040d3f8dd2c4bdf8cda1868b9/ins.sh#L1-L169
+
 #### 作業環境の設定
 この部分ではユーザー体験に直接関わる設定をおこなっています。
 `gsettings`コマンドを使用することにより、使い慣れたUIやキーバインド、各種設定を再現します。
 
 これらをご自身の環境で再現する場合、`dconf watch /`コマンドを使い、設定を変更したときに出力されるコマンドを参考にすると良いでしょう。
 
-```bash
-$ dconf --help
-error: unknown command --help
-
-Usage:
-  dconf COMMAND [ARGS...]
-
-Commands:
-  help              Show this information
-  read              Read the value of a key
-  list              List the contents of a dir
-  write             Change the value of a key
-  reset             Reset the value of a key or dir
-  compile           Compile a binary database from keyfiles
-  update            Update the system databases
-  watch             Watch a path for changes
-  dump              Dump an entire subpath to stdout
-  load              Populate a subpath from stdin
-
-Use 'dconf help COMMAND' to get detailed help.
-
-$ dconf help watch
-Usage:
-  dconf watch PATH 
-
-Watch a path for key changes
-
-Arguments:
-  PATH        Either a KEY or DIR
-  KEY         A key path (starting, but not ending with '/')
-  DIR         A directory path (starting and ending with '/')
-```
 ![](assets/output.gif)
 
+### apt_install_list.sh
+こちらも説明が必要な部分のみ解説します。
+#### Nvidia ドライバーとCUDAライブラリのインストール
+この部分のみ、Ubuntuのバージョンによってインストールするパッケージが異なります。
+```bash
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+```
+このURLは以下のページから取得してください。
+[CUDA Toolkit 12.3 Update 1 Downloads](https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_network)
+
+![](assets/2023-12-14-13-06-04.png)
+
+### VS Code
+ここではマイクロソフトのリポジトリから取得していますが、snapでもインストールできます。その場合は以下のようになります。ただし以前snapバージョンでは日本語の取り回しに難があったため、わたしはdebパッケージを使用しています。
+```bash
+snap install code
+```
+Flatpakでインストールするには以下の手順に沿います。
+```bash
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.visualstudio.code
+```
---
+++ apt_install_list.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+apt update && apt upgrade -y
+
+
+# # ----------------------------------------------
+# # 最小構成
+# # ----------------------------------------------
+apt install -y coreutils
+apt install -y vlc
+apt install -y mcomix
+apt install -y sox
+apt install -y fcitx
+apt install -y fcitx-config-gtk
+apt install -y fcitx-mozc
+apt install -y fcitx-table-emoji
+apt install -y fcitx-tools
+
+
+# # -----------------------------------
+# # FACE01用
+# # -----------------------------------
+apt update && apt upgrade -y
+apt install -y build-essential
+apt install -y cmake
+apt install -y curl
+apt install -y ffmpeg
+apt install -y git
+apt install -y libavcodec-dev
+apt install -y libavformat-dev
+apt install -y libboost-all-dev
+apt install -y libbz2-dev
+
+apt install -y libffi-dev
+apt install -y liblzma-dev
+apt install -y libncurses5-dev
+apt install -y libreadline-dev
+apt install -y libsqlite3-dev
+apt install -y libssl-dev
+apt install -y libswscale-dev
+apt install -y libxml2-dev
+apt install -y libxmlsec1-dev
+
+apt install -y pybind11-dev
+apt install -y python3-dev
+apt install -y python3-tk
+apt install -y python3-venv
+
+apt install -y synaptic
+apt install -y tk-dev
+apt install -y wget
+apt install -y webp
+
+
+# # ----------------------------------------------
+# # Nvidia ドライバーとCUDAライブラリのインストール
+# # ----------------------------------------------
+cd /home/user || exit
+apt install -y cuda-drivers
+# 22.04の場合（ネットワークインストール）
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
+apt update
+apt -y install cuda-toolkit-12-3
+
+# その他ライブラリのインストール
+apt install -y libcudnn8
+apt install -y libcudnn8-dev
+apt install -y libcublas
+apt install -y libcublas-dev
+
+
+# # -----------------------------------
+# # VSCode
+# # -----------------------------------
+if [[ ! -e /home/user/microsoft.gpg ]]; then
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+fi
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+bash -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt update
+apt install -y code
+
+
+# # ----------------------------------------------
+# # ポストインストール
+# # ----------------------------------------------
+apt install -y baobab
+apt install -y catfish
+apt install -y cheese
+apt install -y dconf-editor
+apt install -y fdupes
+apt install -y ffmpegthumbnailer
+apt install -y font-manager
+apt install -y gimp
+apt install -y gnome-system-tools
+apt install -y gnome-tweaks
+apt install -y gnupg-agent
+apt install -y gparted
+apt install -y grsync
+apt install -y gufw
+apt install -y gthumb
+apt install -y handbrake
+apt install -y inkscape
+apt install -y inxi
+apt install -y libreoffice-calc
+apt install -y manpages-ja
+apt install -y meld
+apt install -y mplayer
+apt install -y ncdu
+apt install -y pm-utils
+apt install -y printer-driver-gutenprint
+apt install -y pwgen
+apt install -y rdfind
+apt install -y simple-scan
+apt install -y simplescreenrecorder
+apt install -y thunderbird-gnome-support
+apt install -y translate-shell
+apt install -y transmission-gtk
+apt install -y tree
+apt install -y tumbler-plugins-extra
+apt install -y usb-creator-gtk
+apt install -y xsane
+apt install -y xsel
